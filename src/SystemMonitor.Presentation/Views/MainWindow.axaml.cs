f93aa1da4aaca23b35c3b036da7545040891e94e
AnslmC <anselmocalderon05@gmail.com>
2026-08-07 18:20:38 +0800

    Add DebugSettings for design-time debug borders and update XAML bindings

--- src/SystemMonitor.Presentation/Views/MainWindow.axaml.cs
+++ src/SystemMonitor.Presentation/Views/MainWindow.axaml.cs
@@ -1,6 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
 
 namespace SystemMonitor.Presentation.Views;
 
@@ -9,11 +7,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
-        if (Design.IsDesignMode)
-        {
-            GraphDebugBorder.BorderBrush = Brushes.Red;
-            GraphDebugBorder.BorderThickness = new Thickness(1);
-        }
     }
 }
